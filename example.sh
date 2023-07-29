e9ed16a03f9eb75134a1a7340f31163e0e3a5afc
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-07-29 18:42:14 +0100

    Fixed bugs

--- example.sh
+++ example.sh
@@ -1,2 +1,2 @@
 # pip install jinja2-cli
-jinja2 Dockerfile.jinja alpine_3.18.2_config.json --format=json -D packages="a b c heirloom f rc"
+jinja2 Dockerfile.jinja alpine_3.18.2_config.json --format=json -D packages="a b c heirloom f rc elvish"
