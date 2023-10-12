ceaafff989d8075d3249d2dc9035aa19f5e9433e
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-10-12 17:49:59 +0100

    Passes simple test in Pytest

--- tests/hadolint/test_hadolint_generated_Dockerfiles.py
+++ tests/hadolint/test_hadolint_generated_Dockerfiles.py
@@ -1,53 +1,66 @@
 import subprocess
 import warnings
 import re
+import tempfile
+import pathlib
 
 import pytest
 
 
-def _sys_argv_from_args(args: Iterable[str]) -> tuple[str, subprocess.CompletedProcess]:
+# def _sys_argv_from_args(args: Iterable[str]) -> tuple[str, subprocess.CompletedProcess]:
     
-    # 
-    result = subprocess.run(f'python -X utf8 args.py {" ".join(args)}'
-                           ,shell = True
-                           ,stderr=subprocess.STDOUT
-                           ,stdout = subprocess.PIPE
-                           )
-    output = result.stdout.decode(encoding = 'utf8')
-    return output, result
+#     # 
+#     result = subprocess.run(f'python -X utf8 args.py {' '.join(args)}'
+#                            ,shell = True
+#                            ,stderr=subprocess.STDOUT
+#                            ,stdout = subprocess.PIPE
+#                            )
+#     output = result.stdout.decode(encoding = 'utf8')
+#     return output, result
 
 
 HADOLINT_VERSION = '2.12.0'
 
 result = subprocess.run(['hadolint','--version'], capture_output=True)
 
-output = res.stdout.decode()
+output = result.stdout.decode()
 
-match = re.search(r'\d+\.\d+\.\d+')
+match = re.search(r'\d+\.\d+\.\d+', output)
 
 if match[0] != HADOLINT_VERSION:
-    warnings.warn(f"Dockerfile_generator is currently tested "
-                  f"using Hadolint version {HADOLINT_VERSION}. "
-                  f"Version: {} of Hadolint found "
-                  f"on the path of this environment. "
+    warnings.warn(f'Dockerfile_generator is currently tested '
+                  f'using Hadolint version {HADOLINT_VERSION}. '
+                  f'Version: {match[0]} of Hadolint found '
+                  f'on the path of this environment. '
                  )
 
+TMP_DIR = pathlib.Path(tempfile.gettempdir()) 
+TMP_DOCKERFILE_DIR = TMP_DIR / 'Dockerfile_generator' / 'scratch'
+TMP_DOCKERFILE_DIR.mkdir(exist_ok=True, parents=True)
+TMP_DOCKERFILE_PATH = TMP_DOCKERFILE_DIR / 'Dockerfile'
 
 
-
-
-def _generate_Dockerfile_and_run_hadolint_on_it(config, pkgs, output) -> tuple[str, subprocess.CompletedProcess]:
+def _generate_Dockerfile_and_run_hadolint_on_it(
+    config='debian',
+    pkgs='ash dash zsh heirloom fish elvish',
+    ) -> tuple[str, subprocess.CompletedProcess]:
     
-    result = subprocess.run(HADOLINT_TEST_CMD.format(config=config, params=params)
+    subprocess.run(
+        f'jinja2 Dockerfile.jinja configs/{config}.json --format=json -D packages={pkgs} > {TMP_DOCKERFILE_PATH}',
+        shell = True,
+         )
+
+    result = subprocess.run(f'hadolint {TMP_DOCKERFILE_PATH}'
                            ,shell = True
-                           ,stderr=subprocess.STDOUT
+                           ,stderr = subprocess.STDOUT
                            ,stdout = subprocess.PIPE
                            )
     output = result.stdout.decode(encoding = 'utf8')
     return output, result
 
 
-@pytest.mark.parametrize("config, pkgs, expected_output", [])
+@pytest.mark.parametrize('config, pkgs, expected_output', [('debian', 'ash dash zsh heirloom fish elvish', '')])
 def test_hadolint_Dockerfile(config, pkgs, expected_output):
-    actual_output = _generate_Dockerfile_and_run_hadolint_on_it(config, pkgs, output)
-    assert actual_output == expected_output
+    actual_output, result = _generate_Dockerfile_and_run_hadolint_on_it(config, pkgs)
+    assert result.returncode == 0
+    #assert actual_output == expected_output
