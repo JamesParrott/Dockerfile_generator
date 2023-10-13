78d89983b8a8c25e781f63a7db70c709f6443781
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-10-13 14:38:47 +0100

    Passes 80 tests.  Consolidate RUN statements when using build script.

--- tests/hadolint/test_hadolint_generated_Dockerfiles.py
+++ tests/hadolint/test_hadolint_generated_Dockerfiles.py
@@ -1,10 +1,14 @@
+import os
 import subprocess
 import warnings
 import re
 import tempfile
 import pathlib
+import random
+import json
 
 import pytest
+
 
 
 # def _sys_argv_from_args(args: Iterable[str]) -> tuple[str, subprocess.CompletedProcess]:
@@ -40,27 +44,84 @@ TMP_DOCKERFILE_DIR.mkdir(exist_ok=True, parents=True)
 TMP_DOCKERFILE_PATH = TMP_DOCKERFILE_DIR / 'Dockerfile'
 
 
+ENFORCE_ALL_VERSION_PINNING_WARNINGS = os.getenv('TEST_DOCKERFILE_GENERATOR_ENFORCE_ALL_VERSION_PINNING_WARNINGS', "False") == "True"
+
+VERSION_PINNING_RULES = ('DL3008', 'DL3013', 'DL3016', 'DL3018', 'DL3028')
+#                         Apt-get   Pip       Npm       Apk       Gem
+# E.g: https://github.com/hadolint/hadolint/wiki/DL3008
+
 def _generate_Dockerfile_and_run_hadolint_on_it(
-    config='debian',
-    pkgs='ash dash zsh heirloom fish elvish',
+    config='configs/debian.json',
+    params='ash dash zsh heirloom fish elvish',
+    enforce_version_pinning_warnings = False,
     ) -> tuple[str, subprocess.CompletedProcess]:
     
     subprocess.run(
-        f'jinja2 Dockerfile.jinja configs/{config}.json --format=json -D packages={pkgs} > {TMP_DOCKERFILE_PATH}',
+        f'jinja2 Dockerfile.jinja {config} --format=json -D packages={params} > {TMP_DOCKERFILE_PATH}',
         shell = True,
          )
 
-    result = subprocess.run(f'hadolint {TMP_DOCKERFILE_PATH}'
-                           ,shell = True
-                           ,stderr = subprocess.STDOUT
-                           ,stdout = subprocess.PIPE
-                           )
+    if enforce_version_pinning_warnings or not VERSION_PINNING_RULES:
+        cmd = f'hadolint {TMP_DOCKERFILE_PATH}'
+    else:
+        cmd = f'hadolint --ignore {" --ignore ".join(VERSION_PINNING_RULES)}  {TMP_DOCKERFILE_PATH}'
+
+    result = subprocess.run(cmd,
+                            shell = True,
+                            stderr = subprocess.STDOUT,
+                            stdout = subprocess.PIPE,
+                            )
     output = result.stdout.decode(encoding = 'utf8')
     return output, result
 
 
-@pytest.mark.parametrize('config, pkgs, expected_output', [('debian', 'ash dash zsh heirloom fish elvish', '')])
-def test_hadolint_Dockerfile(config, pkgs, expected_output):
-    actual_output, result = _generate_Dockerfile_and_run_hadolint_on_it(config, pkgs)
+def versioned(pkgs):
+    return all(any(c in pkg for c in ('<','>','=')) for pkg in pkgs)
+
+
+def _generate_test_data(a = 2, b = None, n = 10):
+    for path in pathlib.Path('.').glob('**/*.json'):
+
+        yield str(path), '', True
+
+
+        with open(path, 'rt') as f:
+            config = json.load(f)
+
+
+        params = [param
+                  for command in config.get('COMMANDS', {}).values()
+                  for param in command.get('supported_parameters', []) 
+                 ]
+
+        if not params: 
+            continue
+
+        yield path, ' '.join(params), ENFORCE_ALL_VERSION_PINNING_WARNINGS or versioned(params)
+
+        for param in params:
+            yield path, param, ENFORCE_ALL_VERSION_PINNING_WARNINGS or versioned([param])
+
+
+        max_sample_size = len(params) - 1 if b is None else b
+
+
+        for _ in range(n):
+            try:
+                k = random.randint(a, max_sample_size)
+                sample = random.sample(params, k)
+            except ValueError as e:
+                print(f'{a=}, {max_sample_size=}, {k=}, {path=}, {b=}')
+                raise e
+            yield path, sample, ENFORCE_ALL_VERSION_PINNING_WARNINGS or versioned(sample)
+
+
+
+
+@pytest.mark.parametrize('config, params, enforce_version_pinning_warnings', _generate_test_data()) #[('configs/debian', 'ash dash zsh heirloom fish elvish')])
+def test_config(config, params, enforce_version_pinning_warnings):
+
+    actual_output, result = _generate_Dockerfile_and_run_hadolint_on_it(config, params, enforce_version_pinning_warnings)
+
     assert result.returncode == 0
-    #assert actual_output == expected_output
+    assert 'warning' not in actual_output.lower()
